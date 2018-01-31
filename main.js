0833289c83d55000715b376a1c1e94d103701182
TBNRKirby <tbnrkirby@gmail.com>
2018-01-31 12:43:37 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 var score = 2;
 var scoreText;
 function start(){
+    var background = new WebImage("http://papers.co/wallpaper/papers.co-vy12-simple-white-pattern-background-23-wallpaper.jpg");
+    background.setSize(getWidth(),getHeight());
+    
+    background.setPosition(0,0);
+    add(background);
+    
     var thing = new Rectangle(100,100);
     thing.setPosition(100,100);
     add(thing);
+    
+    var thing2 = new Rectangle(98,98);
+    thing2.setPosition(101,101);
+    thing2.setColor(Color.white);
+    add(thing2);
+    
     mouseClickMethod(clickHandler);
     
     scoreText = new Text(score);
@@ -19,7 +31,7 @@ function text(text){
 
 function clickHandler(e){
 	var elem = getElementAt(e.getX(), e.getY());
-	if(elem != null && elem.getColor() == Color.black){
+	if(elem != null && elem.getColor() == Color.white){
 		score++;
 	}else{
 		score--;
@@ -30,6 +42,7 @@ function clickHandler(e){
 	}
 	if(score == 20){
 		println("You Win :)");
+		score = 0;
 	}
 	
 	scoreText.setText(score);
