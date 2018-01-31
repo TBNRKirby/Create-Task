e74df255c17e2fccf7679e1f61ddc76f5b42d9a5
TBNRKirby <tbnrkirby@gmail.com>
2018-01-31 11:42:41 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-println("Hello World");
-hello
+var score = 2;
+
+function start(){
+    var thing = new Rectangle(100,100);
+    thing.setPosition(100,100);
+    add(thing);
+    mouseClickMethod(goal);
+    
+}
+function text(text){
+	var txt = new Text(text);
+	txt.setPosition(getWidth()/2 - txt.getWidth()/2, 200);
+	add(txt);
+}
+
+function goal(e){
+    
+    var txt = new Text(score);
+	txt.setPosition(e.getX(),e.getY());
+	add(txt);
+    score++;
+}
