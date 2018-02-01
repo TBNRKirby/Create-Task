7cb89e3f80d6321534c0bfd69a233dfb41d19266
TBNRKirby <tbnrkirby@gmail.com>
2018-02-01 12:28:59 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 var score = 2;
 var scoreText;
 function start(){
-    var background = new WebImage("http://papers.co/wallpaper/papers.co-vy12-simple-white-pattern-background-23-wallpaper.jpg");
+    var background = new WebImage("https://cdn.pixabay.com/photo/2017/08/21/11/48/circle-2665079_960_720.jpg");
     background.setSize(getWidth(),getHeight());
     
     background.setPosition(0,0);
     add(background);
     
-    var thing = new Rectangle(100,100);
-    thing.setPosition(100,100);
-    add(thing);
+
     
-    var thing2 = new Rectangle(98,98);
-    thing2.setPosition(101,101);
-    thing2.setColor(Color.white);
-    add(thing2);
+    standard();
+    
+
     
     mouseClickMethod(clickHandler);
     
     scoreText = new Text(score);
 	scoreText.setPosition(0, getHeight());
 	add(scoreText);
+	
+	
 }
 function text(text){
 	var txt = new Text(text);
@@ -47,3 +46,44 @@ function clickHandler(e){
 	
 	scoreText.setText(score);
 }
+function standard(){
+    var box1 = new Rectangle(160,100);
+    var box2 = new Rectangle(160,100);
+    var box3 = new Rectangle(160,100);
+    var box4 = new Rectangle(160,100);
+    box1.setPosition(20,175);
+    box2.setPosition(220,175);
+    box3.setPosition(20,300);
+    box4.setPosition(220,300);
+    add(box1);
+    add(box2);
+    add(box3);
+    add(box4);
+    
+}
+/*
+var x = 20;
+var y = 175;
+function isEven(number){
+    if(number % 2 == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+var box = [];
+for(var i = 0;i<4;i++){
+    var box = new Rectangle(160,100);
+    var even = ifEven(i);
+    if (even == false){
+        box.setPosition(x,y);
+        if(i>2){
+            box.setPosition(x,y+125);
+        }
+    }else{
+        
+    }
+}
+*/
+
+
